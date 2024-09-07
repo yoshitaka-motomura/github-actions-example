c38e81a93a1694800a3eeb87d360e13b7e26e4dd
Yoshitaka motomura <stay.daybreak@gmail.com>
2024-09-08 01:18:40 +0900

    Refactor CDK stack to create a versioned S3 bucket and add a DynamoDB table
    - Refactor CDK stack to create a versioned S3 bucket
    - Add DynamoDB table construct to the CDK stack
    - Update test directory path in jest.config.js
    - Ignore localstack volume in .gitignore
    - Add wheel target for building package
    - Create cdk APIGATEWAY

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,11 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const { account, region } = cdk.Stack.of(this)
+
+    /**
+     * S3 Bucket Construct
+     */
     const Bucket = new s3.Bucket(this, 'ServiceBucket', {
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
@@ -15,9 +20,28 @@ export class CdkStack extends cdk.Stack {
       bucketName: `input-bucket-${region}-${account}`,
     })
 
+    /**
+    * Dynamodb Table Construct
+    */
+
+    const memoTable = new dynamodb.Table(this, 'MemoTable', {
+      tableName: 'Memo',
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    })
+
+
+    /**
+    * Output
+    **/
     new cdk.CfnOutput(this, 'BucketName', {
       value: Bucket.bucketName,
       description: 'The name of the S3 bucket',
     });
+    new cdk.CfnOutput(this, 'MemoTableName', {
+      value: memoTable.tableName,
+      description: 'The name of the DynamoDB table',
+    })
   }
 }
--- tests/cdk.test.ts
+++ tests/cdk.test.ts
@@ -1,17 +1,38 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as Cdk from '../lib/cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+const app = new cdk.App();
+const stack = new Cdk.CdkStack(app, 'MyTestStack');
+const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+describe('CDK Stack Resources Tests', () =>{
+    test('S3 Bucket Created', () => {
+        expect(template.resourceCountIs('AWS::S3::Bucket', 1));
+        expect(template.hasResourceProperties('AWS::S3::Bucket', {
+        VersioningConfiguration: {
+            Status: 'Enabled',
+        },
+        PublicAccessBlockConfiguration: {
+            BlockPublicAcls: true,
+            BlockPublicPolicy: true,
+            IgnorePublicAcls: true,
+            RestrictPublicBuckets: true,
+        },
+        }));
+    });
+
+    test('DynamoDB Table Created', () => {
+        expect(template.resourceCountIs('AWS::DynamoDB::Table', 1));
+        expect(template.hasResourceProperties('AWS::DynamoDB::Table', {
+        KeySchema: [
+            {
+            AttributeName: 'id',
+            KeyType: 'HASH',
+            },
+        ],
+        BillingMode: 'PAY_PER_REQUEST',
+        }));
+    })
 });
+
