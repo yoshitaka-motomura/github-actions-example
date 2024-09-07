744f3659471893d308d85ffd566332e34efa9eca
Yoshitaka motomura <stay.daybreak@gmail.com>
2024-09-08 00:30:43 +0900

    Refactor CDK stack to create a versioned S3 bucket
    - Updated the CDK stack to create a versioned S3 bucket with specific configurations.
    - Removed unnecessary code related to SQS.

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,23 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import * as s3 from 'aws-cdk-lib/aws-s3';
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const { account, region } = cdk.Stack.of(this)
+    const Bucket = new s3.Bucket(this, 'ServiceBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      publicReadAccess: false,
+      autoDeleteObjects: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      bucketName: `input-bucket-${region}-${account}`,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, 'BucketName', {
+      value: Bucket.bucketName,
+      description: 'The name of the S3 bucket',
+    });
   }
 }
